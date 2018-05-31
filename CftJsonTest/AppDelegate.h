b6ddae87fda52b02f1b3abbe291d4dcaeb39f389
Dmitry Edrenov <edrenoff@icloud.com>
2018-05-31 22:19:19 +0600

    Initial Commit

---
+++ CftJsonTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CftJsonTest
+//
+//  Created by Dmitry Edrenov on 31.05.18.
+//  Copyright © 2018 Dmitry Edrenov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CftJsonTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CftJsonTest
+//
+//  Created by Dmitry Edrenov on 31.05.18.
+//  Copyright © 2018 Dmitry Edrenov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
